323aa9a0e8d5f5bfa36c33e8bd9ec037efeacb37
jordanlev94 <66325064+jordanlev94@users.noreply.github.com>
2020-09-07 00:59:28 +0300

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styles from './Footer.module.css'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+       <div className='Footer'>
+
+        <div className={styles.container}>
+
+           <div className="glbfooter">
+
+            <div className='footer1'>
+            Contact
+
+        Association Keren Or Yossef Itshak
+        ​​​​​​​Laurent Krief
+
+​​​​​​​          France : 01 77 50 06 44
+
+        Israël : 0526029631
+            </div>
+
+            <div className='footer2'>
+            Informations génerales
+
+         Accueil 
+            autre 
+autre 
+autre 
+autre
+        
+            </div>
+            
+        </div>
+        </div>
+        </div>
+    )
+}
+
+export default Footer
+
